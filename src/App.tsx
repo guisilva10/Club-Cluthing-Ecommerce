45a782a939f50880d20d7323193178c80673c5e3
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-09-02 14:01:13 -0300

    feat: add React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from 'react'
 import HomePage from './pages/home/home.page'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Components
 
 const App: FunctionComponent = () => {
   return (
-    <HomePage/>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<HomePage/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
