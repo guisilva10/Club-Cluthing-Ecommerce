b54e126ac2fe41f904b067e76d5fc9224cf85dd4
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-09-04 02:49:38 -0300

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#f8f9fa',
+    dark: '#343a40'
+  },
+  background: {
+    dark: '#212529',
+    white: '#f8f9fa'
+  },
+  inpurt: {
+    background: '#e9ecef',
+    placeholder: '#6c757d'
+  },
+  error: '#ff6a4a'
+}
+
+export default Colors
