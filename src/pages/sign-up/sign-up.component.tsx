39ae6877881845bcd274877a3d11485a7a645fc5
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-09-09 15:25:41 -0300

    feat: add user account creation

--- src/pages/sign-up/sign-up.component.tsx
+++ src/pages/sign-up/sign-up.component.tsx
@@ -15,9 +15,12 @@ import {
   SignUpHeadline,
   SignUpInputContainer
 } from './sign-up.styles'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth, db } from '../../config/firebase.config'
+import { addDoc, collection } from 'firebase/firestore'
 
 interface SignUpForm {
-  name: string
+  firstName: string
   lastName: string
   email: string
   password: string
@@ -34,8 +37,19 @@ const SignUpPage = () => {
 
   const watchPassword = watch('password')
 
-  const handleSubmitPress = (data: SignUpForm) => {
-    console.log({ data })
+  const handleSubmitPress = async (data: SignUpForm) => {
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(auth, data.email, data.password)
+
+      await addDoc(collection(db, 'users'), {
+        id: userCredentials.user.uid,
+        email: userCredentials.user.email,
+        firstName: data.firstName,
+        lastName: data.lastName
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -49,12 +63,12 @@ const SignUpPage = () => {
           <SignUpInputContainer>
             <p>Nome</p>
             <CustomInput
-              hasError={!!errors?.name}
+              hasError={!!errors?.firstName}
               placeholder="Digite seu nome"
-              {...register('name', { required: true })}
+              {...register('firstName', { required: true })}
             />
 
-            {errors?.name?.type === 'required' && (
+            {errors?.firstName?.type === 'required' && (
               <InputErrorMessage>O nome é obrigatório.</InputErrorMessage>
             )}
           </SignUpInputContainer>
