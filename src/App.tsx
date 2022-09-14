ab78788985d98087aae9dd47f1ae71edf072de29
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-09-14 20:50:46 -0300

    feat: add initializing state to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useContext } from 'react'
+import { FunctionComponent, useContext, useState } from 'react'
 import HomePage from './pages/home/home.page'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/login/login.page'
@@ -11,12 +11,14 @@ import { UserContext } from './contexts/user.context'
 import { collection, getDocs, query, where } from 'firebase/firestore'
 
 const App: FunctionComponent = () => {
+  const [isInitializing, setIsInitializing] = useState(true)
   const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
 
   onAuthStateChanged(auth, async (user) => {
     const isSignInOut = isAuthenticated && !user
     if (isSignInOut) {
-      return logoutUser()
+      logoutUser()
+      return setIsInitializing(false)
     }
 
     const isSigningIn = !isAuthenticated && user
@@ -25,9 +27,14 @@ const App: FunctionComponent = () => {
         query(collection(db, 'users'), where('id', '==', user.uid))
       )
       const userFromFirestore = querySnapshot.docs[0]?.data()
-      return loginUser(userFromFirestore as any)
+
+      loginUser(userFromFirestore as any)
+
+      return setIsInitializing(false)
     }
+    return setIsInitializing(false)
   })
+  if (isInitializing) return null
   return (
    <BrowserRouter>
    <Routes>
