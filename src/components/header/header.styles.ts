bce325f20c03fe4f559c047a431bb5996b0a8634
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-09-14 20:56:47 -0300

    feat: add home navigation to logo click

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -13,6 +13,10 @@ color: ${Colors.text.white};
 export const HeaderTitle = styled.h2`
  font-weight: bold;
  font-size: 1.5rem;
+ 
+ &&:hover{
+  cursor: pointer;
+ }
 `
 
 export const HeaderItems = styled.div`
