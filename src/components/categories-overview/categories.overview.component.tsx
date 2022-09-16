f8eb09fefb46d475c50b8dbde989903370ccf27e
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-09-16 14:49:16 -0300

    feat: add Loading to Explorer Page

--- src/components/categories-overview/categories.overview.component.tsx
+++ src/components/categories-overview/categories.overview.component.tsx
@@ -6,15 +6,18 @@ import { Container } from './categories.overview.styles'
 // utilities
 import { CategoryContext } from '../../contexts/categories.context'
 import CategoryOverview from '../category-overview/category-overview.component'
+import Loading from '../loading/loading.component'
 
 const CategoriesOverview: FunctionComponent = () => {
-  const { categories, fetchCategories } = useContext(CategoryContext)
+  const { categories, isLoading, fetchCategories } = useContext(CategoryContext)
 
   useEffect(() => {
     if (categories.length === 0) {
       fetchCategories()
     }
   }, [])
+
+  if (isLoading) return <Loading/>
 
   return <Container>
     {categories.map((category) => (
