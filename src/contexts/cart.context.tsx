590b3287bc491487b24c596edd9f429eee8b250b
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-09-19 16:21:04 -0300

    feat: add Cart Context

---
+++ src/contexts/cart.context.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, FunctionComponent, useState } from 'react'
+import CartProduct from '../types/cart.types'
+
+interface PropsChildren{
+  children: React.ReactNode
+}
+
+interface ICartContext {
+  isVisible: boolean
+  products: CartProduct[]
+  toggleCart: () => void
+}
+
+const CartContext = createContext<ICartContext>({
+  isVisible: false,
+  products: [],
+  toggleCart: () => {}
+
+})
+
+const CartContextProvider: FunctionComponent<PropsChildren> = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [products] = useState<CartProduct[]>([])
+  const toggleCart = () => {
+    setIsVisible(prevState => !prevState)
+  }
+
+  return (
+    <CartContext.Provider value={{ isVisible, products, toggleCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartContextProvider
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import UserContextProvider from './contexts/user.context'
 import CategoryContextProvider from './contexts/categories.context'
+import CartContextProvider from './contexts/cart.context'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,13 +14,12 @@ root.render(
   <React.StrictMode>
     <UserContextProvider>
       <CategoryContextProvider>
-       <App />
+        <CartContextProvider>
+        <App />
+        </CartContextProvider>
       </CategoryContextProvider>
     </UserContextProvider>
   </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
