95d80a62ef442fe9f2805439e2ebc5f685e35f30
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-10-03 05:06:57 -0300

    feat: add stripe api integration to checkout

--- src/components/checkout/checkout.component.tsx
+++ src/components/checkout/checkout.component.tsx
@@ -1,16 +1,38 @@
-import { FunctionComponent, useContext } from 'react'
+import axios from 'axios'
+import { FunctionComponent, useContext, useState } from 'react'
 import { BsBagCheck } from 'react-icons/bs'
 
 import { CartContext } from '../../contexts/cart.context'
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component'
+import Loading from '../loading/loading.component'
 
 import { CheckoutContainer, CheckoutProducts, CheckoutTitle, CheckoutTotal } from './checkout.styles'
 
 const Checkout: FunctionComponent = () => {
   const { products, productsTotalPrice } = useContext(CartContext)
 
-  return <CheckoutContainer>
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFinishPurchaseClick = async () => {
+    try {
+      setIsLoading(true)
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}/create-checkout-session`,
+
+        { products })
+
+      window.location.href = data.url
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return <>
+  {isLoading && <Loading/>}
+  <CheckoutContainer>
     <CheckoutTitle>Checkout</CheckoutTitle>
 
     {products.length > 0
@@ -24,7 +46,7 @@ const Checkout: FunctionComponent = () => {
 
     <CheckoutTotal>Total: R${productsTotalPrice}</CheckoutTotal>
 
-    <CustomButton startIcon={<BsBagCheck/>}>Finalizar Compra</CustomButton>
+    <CustomButton startIcon={<BsBagCheck/>} onClick={handleFinishPurchaseClick}>Finalizar Compra</CustomButton>
       </>
         )
       : (
@@ -33,6 +55,7 @@ const Checkout: FunctionComponent = () => {
     }
 
     </CheckoutContainer>
+  </>
 }
 
 export default Checkout
